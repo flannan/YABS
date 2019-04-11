ae913b2ee61542bdc5615eb1e46c90226481416d
flannan <flannan383@gmail.com>
2019-04-11 18:03:27 +0300

    Started adding Api.

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 declare(strict_types=1);
 
-include_once __DIR__ . 'customersApi.php';
+include_once __DIR__ . '/customersApi.php';
+include_once __DIR__ . '/Api.php';
+include_once __DIR__ . '/Customer.php';
+include_once __DIR__ . '/Database.php';
+include_once __DIR__ . '/User.php';
 
 try {
     $api = new flannan\YABS\customersApi();
